67894ae6570b8e20bf67f5063afb039d9b1d3cbb
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-24 03:30:24 -0400

    commit tree abb6ce4e390995853885ca0274205942d9a01131

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(arry, func) {
+  let newArry = []
+  for (let i = 0; i < arry.length; i++) {
+    newArry.push(func(arry[i]));
+  }
+  return newArry;
+}
+
+function reduce(arry, func, stValue) {
+  let newValue;
+  if (stValue) {
+    newValue = stValue
+  for (let i = 0; i < arry.length; i++) {
+    newValue = func(arry[i], newValue); 
+  }
+  return newValue; 
+  }
+  else {
+    newValue = arry[0]
+  for (let i = 1; i < arry.length; i++) {
+    newValue = func(arry[i], newValue); 
+  }
+  return newValue
+}
+}
